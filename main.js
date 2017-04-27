4ea16b15beab888f1284fd587dd6e58b309b70d6
wolflint <marcinwolf363@gmail.com>
2017-04-27 11:19:28 +0100

    Created functions

--- main.js
+++ main.js
@@ -3,16 +3,53 @@ var mainStage = {
   preload: function() {
     //This function will be executed at the beginning
     //That's where we load the images and sound
+
+    //Load the bird sprite
+    game.load.image('bird', 'assets/bird.png');
   },
 
   create: function() {
       //This function is called after the preload function
       //Here we setup the game, display sprites, etc...
+
+      //Change the background colour o the game to ble - for now!
+      game.stage.backgroundColor = '#71c5cf';
+
+      //Set the physics for the game
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+
+      //Display the bird as the position of x=100 and y=245
+      this.bird = game.add.sprite(100, 245, 'bird');
+
+      //Add physics to the bird
+      //Needed for: movement, gravity, collisions, etc...
+      game.physics.arcade.enable(this.bird);
+
+      //Add gravity to the bird to ake it fall
+      this.bird.body.gravity.y = 1000;
+
+      //Call 'jump' function when the space bar is pressed
+      var spaceBar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      spaceBar.onDown.add(this.jump, this);
   },
 
     update: function() {
         //This function is called 60 times per second
         //It contains the games logic
+
+        //Call the 'restartGame' function
+        if (this.bird.y <0 || this.bird.y > 490)
+        this.restartGame();
+    },
+    jump: function() {
+      //Add a vertical velocity to the bird
+      this.bird.body.velocity.y = -350;
+    },
+
+    //Retart the game
+    restartGame: function() {
+      //Start the 'main' state which restarts game
+      game.state.start('main');
     },
 };
 
